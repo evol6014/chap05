52b42edfa958792a31b250af98dc900bde5bb7c2
java <java@bigdata>
2017-09-20 12:46:08 +0900

    jsp dept

---
+++ src/main/resources/sql/mysql_study.sql
@@ -0,0 +1,28 @@
+
+select * from dept;
+select * from emp;
+
+update emp
+   set gender = 'F'
+ where gender = 'f';
+commit; 
+
+select * from tbl_dept;
+select * from tbl_emp;
+
+insert into tbl_dept
+select * from dept;
+
+insert into tbl_emp
+(comm, deptno, empno, ename, gender, hiredate, job, mgr, sal)
+select comm, deptno, empno, ename, gender, hiredate, job, mgr, sal from emp;
+
+insert into tbl_profile 
+(fname, curr)
+values
+("user01_profile", 1);
+
+select * from tbl_profile;
+
+
+ 
---
+++ src/main/webapp/WEB-INF/jsp/dept/list.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="/webjars/bootstrap/3.3.7/css/bootstrap.css">
+<link rel="stylesheet" href="/webjars/animate.css/3.5.2/animate.css">
+<title>list.jsp</title>
+</head>
+<body>
+<h1>Dept List</h1>
+${depts}
+
+</body>
+</html>
